9a5fe9a64e62fbdbd20187ede5f4ca31ff55b655
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-15 22:41:51 -0700

    first commit

--- lib/controller/ui_controller/auth/sign_in.dart
+++ lib/controller/ui_controller/auth/sign_in.dart
@@ -1,5 +1,15 @@
-
-
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:myapp/controller/api_controller/auth/sign_in.dart';
 
-class SignInController extends GetxController{}
+class SignInController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  RxBool isLoading = true.obs;
+
+  signinFun() async {
+    bool status = await SignInService.signinService(
+        email: emailController.text, password: passwordController.text);
+  }
+}
