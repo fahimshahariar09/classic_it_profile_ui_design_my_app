ea3b9fe59f31055862c30aa296f2b4a78cfef6a4
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-15 21:32:04 -0700

    first commit

--- lib/view/common_widget/common_button.dart
+++ lib/view/common_widget/common_button.dart
@@ -1,13 +1,19 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CommonButton extends StatelessWidget {
   const CommonButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: MediaQuery.sizeOf(context).width,
+    return InkWell(
+      child: Card(
+        color: Colors.blue,
+        child: SizedBox(
+          height: 50,
+          width: MediaQuery.sizeOf(context).width,
+          child: Text("data"),
+        ),
+      ),
     );
   }
 }
