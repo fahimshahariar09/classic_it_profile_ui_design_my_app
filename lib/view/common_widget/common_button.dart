88ed800658550cac8fae85e7e66b6f148b555e91
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-15 21:56:19 -0700

    first commit

--- lib/view/common_widget/common_button.dart
+++ lib/view/common_widget/common_button.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/view/common_widget/common_text.dart';
 
 class CommonButton extends StatelessWidget {
-  const CommonButton({super.key});
+  const CommonButton(
+      {super.key,
+      required this.buttonName,
+      this.buttonHeight,
+      this.buttonWidth,
+      this.buttonColor,
+      this.textColor,
+      required this.onTap});
+
+  final String buttonName;
+  final double? buttonHeight;
+  final double? buttonWidth;
+  final Color? buttonColor;
+  final Color? textColor;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: onTap,
       child: Card(
-        color: Colors.blue,
+        color: buttonColor ?? Colors.blue,
         child: SizedBox(
-          height: 50,
-          width: MediaQuery.sizeOf(context).width,
-          child: Text("data"),
+          height: buttonHeight ?? 50,
+          width: buttonWidth ?? MediaQuery.sizeOf(context).width,
+          child: Center(child: CommonText(titel: buttonName)),
         ),
       ),
     );
