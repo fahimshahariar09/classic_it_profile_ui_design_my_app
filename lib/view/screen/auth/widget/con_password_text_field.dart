5865d597ba9feddd8da1cc6af104997e5089fdb6
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-16 22:50:22 -0700

    first commit

--- lib/view/screen/auth/widget/con_password_text_field.dart
+++ lib/view/screen/auth/widget/con_password_text_field.dart
@@ -5,6 +5,31 @@ class ConPasswordTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return TextFormField(
+      decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: Colors.red,
+              width: 1,
+            ),
+          ),
+          labelText: "Password"),
+      validator: (value) {
+        if (value == '' || value == null) {
+          return "Password can't be empty";
+        }
+        return null;
+      },
+    );
   }
 }
