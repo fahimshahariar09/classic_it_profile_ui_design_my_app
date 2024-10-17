4e2fde0c18657396fb37f7a7d7f9889d226f56d0
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-16 22:50:03 -0700

    first commit

---
+++ lib/view/screen/auth/widget/con_password_text_field.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConPasswordTextField extends StatelessWidget {
+  const ConPasswordTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField();
+  }
+}
