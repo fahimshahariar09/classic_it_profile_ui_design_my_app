0a4a6356f11a51bf93dfe18c494f227ad6037f6e
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-16 22:34:31 -0700

    first commit

--- lib/controller/ui_controller/auth/sign_in.dart
+++ lib/controller/ui_controller/auth/sign_in.dart
@@ -31,5 +31,4 @@ class SignInController extends GetxController {
     passwordController.dispose();
     super.dispose();
   }
-
 }
--- lib/view/screen/auth/sign_in.dart
+++ lib/view/screen/auth/sign_in.dart
@@ -7,6 +7,7 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
