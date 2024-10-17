efae90fc02b8717f93417adf89567fc6542959e7
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-16 22:54:52 -0700

    first commit

---
+++ lib/controller/ui_controller/auth/sign_up.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class SignUpController extends GetxController{
+
+  TextEditingController nameController =TextEditingController();
+  TextEditingController emailController =TextEditingController();
+  TextEditingController passwordController =TextEditingController();
+  TextEditingController conpasswordController =TextEditingController();
+
+}
--- lib/view/screen/auth/sign_up.dart
+++ lib/view/screen/auth/sign_up.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/view/screen/auth/widget/email_text_field.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          //EmailTextField(emailController: emailController)
+        ],
+      ),
+    );
   }
 }
