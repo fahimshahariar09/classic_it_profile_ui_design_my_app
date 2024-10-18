626f231f9d489874f56b5603da946c2a1e614370
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-17 23:03:52 -0700

    first commit

--- lib/controller/ui_controller/auth/sign_up.dart
+++ lib/controller/ui_controller/auth/sign_up.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 
 class SignUpController extends GetxController{
 
+  final formKey = GlobalKey<FormState>();
   TextEditingController nameController =TextEditingController();
   TextEditingController emailController =TextEditingController();
   TextEditingController passwordController =TextEditingController();
--- lib/view/screen/auth/sign_up.dart
+++ lib/view/screen/auth/sign_up.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapp/controller/ui_controller/auth/sign_up.dart';
+import 'package:myapp/view/common_widget/common_button.dart';
+import 'package:myapp/view/screen/auth/widget/con_password_text_field.dart';
 import 'package:myapp/view/screen/auth/widget/email_text_field.dart';
 import 'package:myapp/view/screen/auth/widget/name_text_feild.dart';
 import 'package:myapp/view/screen/auth/widget/password_text_field.dart';
@@ -12,16 +14,24 @@ class SignUp extends StatelessWidget {
   Widget build(BuildContext context) {
     SignUpController signUpController = Get.put(SignUpController());
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 20),
-          NameTextFeild(nameController: signUpController.nameController),
-          SizedBox(height: 15),
-          EmailTextField(emailController: signUpController.emailController),
-          SizedBox(height: 15),
-          PasswordTextField(passwordController: signUpController.passwordController),
-          SizedBox(height: 15),
-        ],
+      body: Form(
+        key: signUpController.formKey,
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            NameTextFeild(nameController: signUpController.nameController),
+            SizedBox(height: 15),
+            EmailTextField(emailController: signUpController.emailController),
+            SizedBox(height: 15),
+            PasswordTextField(
+                passwordController: signUpController.passwordController),
+            SizedBox(height: 15),
+            ConPasswordTextField(
+                conpasswordController: signUpController.conpasswordController),
+            SizedBox(height: 15),
+            CommonButton(buttonName: "Sign Up", onTap: () {}),
+          ],
+        ),
       ),
     );
   }
