557c9309a860c0b70b0c6b82f2db6cd117b6e59b
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-17 23:32:32 -0700

    first commit

--- lib/controller/ui_controller/auth/forget_password.dart
+++ lib/controller/ui_controller/auth/forget_password.dart
@@ -1,5 +1,7 @@
-
-
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class ForgetPasswordController extends GetxController{}
+class ForgetPasswordController extends GetxController {
+  TextEditingController emailController = TextEditingController();
+  RxBool isLoading = true.obs;
+}
