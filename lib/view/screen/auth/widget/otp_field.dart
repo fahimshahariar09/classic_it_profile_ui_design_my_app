4c2f94b3038e5784a40cdf01b9addefad97a56a1
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-18 23:14:24 -0700

    first commit

--- lib/view/screen/auth/widget/otp_field.dart
+++ lib/view/screen/auth/widget/otp_field.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class OtpField extends StatelessWidget {
@@ -8,11 +7,18 @@ class OtpField extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 50,
-      width: 80,
+      width: 50,
       child: TextField(
         decoration: InputDecoration(
-          focusedBorder: OutlineInputBorder(),
-          enabledBorder: OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(color: Colors.grey),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(color: Colors.grey),
+          ),
+          fillColor: Colors.grey,
         ),
       ),
     );
