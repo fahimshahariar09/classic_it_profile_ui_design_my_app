8dc7fa09719f6f77485eacd585bc770cdd56f32e
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-18 23:05:02 -0700

    first commit

---
+++ lib/view/screen/auth/widget/otp_field.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class OtpField extends StatelessWidget {
+  const OtpField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 80,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
+  }
+}
