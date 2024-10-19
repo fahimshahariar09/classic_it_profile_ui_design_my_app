85b2e777db9f6671a2ea25898ae4f610eaf84689
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-18 23:02:47 -0700

    first commit

--- lib/view/screen/auth/forget_password.dart
+++ lib/view/screen/auth/forget_password.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:myapp/controller/ui_controller/auth/forget_password.dart';
+import 'package:myapp/view/screen/auth/widget/email_text_field.dart';
 
 class ForgetPassword extends StatelessWidget {
   const ForgetPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    ForgetPasswordController forgetController = Get.put(ForgetPasswordController());
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(height: 15),
+          EmailTextField(emailController: forgetController.emailController),
+        ],
+      ),
+    );
   }
 }
