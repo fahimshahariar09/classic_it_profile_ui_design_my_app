cb398256d04794080a3a37a848e42f7c093445d0
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-19 23:09:50 -0700

    first commit

--- lib/controller/ui_controller/auth/otp.dart
+++ lib/controller/ui_controller/auth/otp.dart
@@ -1,4 +1,12 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class OtpController extends GetxController{}
+class OtpController extends GetxController{
+
+  TextEditingController numConroller = TextEditingController();
+  TextEditingController num1Conroller = TextEditingController();
+  TextEditingController num2Conroller = TextEditingController();
+  TextEditingController num3Conroller = TextEditingController();
+
+}
