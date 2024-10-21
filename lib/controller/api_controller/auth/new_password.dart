95467b1a2fe8fff2135a7a644406a737f2c11f0f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-20 23:13:19 -0700

    first commit

--- lib/controller/api_controller/auth/new_password.dart
+++ lib/controller/api_controller/auth/new_password.dart
@@ -1,2 +1,10 @@
 
-class NewPasswordService{}
+import 'dart:developer';
+
+class NewPasswordService{
+
+  static Future newpasswordService()async{
+    try{}catch (e){log("error $e");}return false;
+  }
+
+}
