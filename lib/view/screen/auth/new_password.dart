58783aa5fe724b544bca3d0024aaa7af23968c61
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-20 23:27:25 -0700

    first commit

--- lib/view/screen/auth/new_password.dart
+++ lib/view/screen/auth/new_password.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:myapp/controller/ui_controller/auth/new_password.dart';
+import 'package:myapp/view/screen/auth/widget/password_text_field.dart';
 
 class NewPassword extends StatelessWidget {
   const NewPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    NewPasswordController controller =Get.put(NewPasswordController());
+    return Scaffold(
+      body: Column(
+        children: [
+          PasswordTextField(passwordController: controller.oldpassword),
+        ],
+      ),
+    );
   }
 }
