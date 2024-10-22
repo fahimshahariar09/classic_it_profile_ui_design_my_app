28fe582319f4b84fd87c9ef5bbe351dca91a8c09
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-21 23:30:59 -0700

    first commit

--- lib/view/screen/auth/verification.dart
+++ lib/view/screen/auth/verification.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/view/common_widget/common_text.dart';
+import 'package:myapp/view/common_widget/custom_timer.dart';
 import 'package:myapp/view/screen/auth/widget/otp_field.dart';
 
 class Verification extends StatelessWidget {
@@ -7,21 +8,21 @@ class Verification extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return  const Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
+          padding: EdgeInsets.symmetric(horizontal: 15),
           child: Column(
             children: [
-              const SizedBox(height: 20),
-              const CommonText(titel: "OTP VERIFICATION "),
-              const SizedBox(height: 10),
-              const CommonText(
+              SizedBox(height: 20),
+              CommonText(titel: "OTP VERIFICATION "),
+              SizedBox(height: 10),
+              CommonText(
                 titel: "Please enter the verification code sent to :",
                 fColor: Colors.grey,
               ),
-              const CommonText(titel: "fahim@gmail.com"),
-              const SizedBox(
+              CommonText(titel: "fahim@gmail.com"),
+              SizedBox(
                 height: 30,
               ),
               SizedBox(height: 25),
@@ -36,8 +37,11 @@ class Verification extends StatelessWidget {
                   SizedBox(width: 20),
                   OtpField(),
                 ],
-              ),SizedBox(height: 15),
-              const Row(
+              ),
+              SizedBox(height: 15),
+              CustomTimer(),
+              SizedBox(height: 15),
+              Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
