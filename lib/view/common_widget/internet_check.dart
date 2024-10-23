d0340ffe9e5acba5251337692f8ea0b5a4f85fc0
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-22 23:58:23 -0700

    first commit

--- lib/view/common_widget/internet_check.dart
+++ lib/view/common_widget/internet_check.dart
@@ -5,6 +5,12 @@ class InternetCheck extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
