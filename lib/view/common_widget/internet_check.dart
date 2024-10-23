7cd0c46cd25927f0cfd2f97cc75f84167a9e4274
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-22 23:58:07 -0700

    first commit

---
+++ lib/view/common_widget/internet_check.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InternetCheck extends StatelessWidget {
+  const InternetCheck({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
