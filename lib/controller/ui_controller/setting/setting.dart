3527a79ba09ff401c48d71f7dc6c1769af70d22b
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-25 23:36:02 -0700

    first commit

--- lib/controller/ui_controller/setting/setting.dart
+++ lib/controller/ui_controller/setting/setting.dart
@@ -1,5 +1,13 @@
 
 
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SettingController extends GetxController{}
+class SettingController extends GetxController{
+  RxBool isLightTheme = true.obs;
+
+  changeTheme()async{
+    isLightTheme.value =!isLightTheme.value;
+    Get.changeTheme(!isLightTheme.value ? ThemeData.dark() :ThemeData.light());
+  }
+}
