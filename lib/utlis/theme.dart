310aa43f92beb067878fd3b99d27097d8a496ba8
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-25 23:30:33 -0700

    first commit

--- lib/utlis/theme.dart
+++ lib/utlis/theme.dart
@@ -2,5 +2,15 @@
 import 'package:flutter/material.dart';
 
 class AppTheme{
-  ThemeData lightTheme = ThemeData();
+  ThemeData lightTheme = ThemeData(
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.w500,
+        fontSize: 25,
+      )
+    )
+  );
 }
