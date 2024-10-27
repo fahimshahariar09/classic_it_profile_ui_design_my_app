5516ea9e01316573b91afc40909a1c6ea90d50f8
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-26 23:51:52 -0700

    first commit

--- lib/controller/ui_controller/setting/setting.dart
+++ lib/controller/ui_controller/setting/setting.dart
@@ -4,8 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SettingController extends GetxController{
+  //darkmode
   RxBool isLightTheme = true.obs;
+  //language
+  RxString selectedLanguage ='BD'.obs;
+  RxList<String> languageList =<String>[].obs;
 
+  //darkmode
   changeTheme()async{
     isLightTheme.value =!isLightTheme.value;
     Get.changeTheme(!isLightTheme.value ? ThemeData.dark() :ThemeData.light());
