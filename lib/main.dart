03b6b54cf931a6539d316c74a46c0fb91e0e8e44
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-27 23:20:38 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/utlis/theme.dart';
-import 'package:myapp/view/screen/payment/ssl_commerz.dart';
-import 'package:myapp/view/setting/setting.dart';
+import 'package:myapp/view/screen/setting/setting.dart';
 
 void main() {
   runApp(const MyClass());
