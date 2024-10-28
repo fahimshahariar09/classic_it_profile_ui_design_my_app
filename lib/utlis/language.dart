c6266c82679b36776a79955ee107eb4e647d9c7b
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-27 23:19:49 -0700

    first commit

--- lib/utlis/language.dart
+++ lib/utlis/language.dart
@@ -2,5 +2,17 @@ import 'package:get/get_navigation/src/root/internacionalization.dart';
 
 class AppLanguage extends Translations {
   @override
-  Map<String, Map<String, String>> get keys => {};
+  Map<String, Map<String, String>> get keys => {
+        //english language
+        "en_us": {"home_screen": "Home Screen", "profile": "Profile"},
+
+        //bangla language
+        "bn_BD": {
+          "home screen": "হোম স্ক্রীন",
+          "profile": "প্রোফাইল",
+        },
+
+        //hindi language
+        "hi_IN": {"home_screen": "होम स्क्रीन", "profile": "प्रोफ़ाइल"}
+      };
 }
