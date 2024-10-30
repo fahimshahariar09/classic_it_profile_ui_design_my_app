fa042a94d810906cb09fdc1c8c315b2b8bf5e54f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-29 23:25:16 -0700

    first commit

--- lib/view/screen/profile/profile.dart
+++ lib/view/screen/profile/profile.dart
@@ -10,13 +10,19 @@ class Profile extends StatelessWidget {
       body: Column(
         children: [
           CircleAvatar(radius: 50,),
+          SizedBox(height: 10),
           CommonButton(buttonName: "Save", onTap: (){}),
+          SizedBox(height: 10),
           Row(
             children: [
               Text("first name"),
               Text("first name"),
             ],
-          )
+          ),
+          SizedBox(height: 10),
+          Text("phone"),
+          SizedBox(height: 10),
+          Text("email"),
         ],
       ),
     );
