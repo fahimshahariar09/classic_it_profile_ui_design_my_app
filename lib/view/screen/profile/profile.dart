a1c3619b050ef066c866fa9375c958a3c6a97ac9
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-29 23:21:22 -0700

    first commit

--- lib/view/screen/profile/profile.dart
+++ lib/view/screen/profile/profile.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/view/common_widget/common_button.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          CircleAvatar(radius: 50,),
+          CommonButton(buttonName: "Save", onTap: (){}),
+        ],
+      ),
+    );
   }
 }
