5176b70b023cc58cb538e93bec9113f6595dfb60
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-30 23:33:37 -0700

    first commit

---
+++ lib/view/screen/profile/widget/common_field.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CommonField extends StatelessWidget {
+  const CommonField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(),
+    );
+  }
+}
