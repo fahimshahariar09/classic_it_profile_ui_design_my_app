5ee4b131bc61951477889928b1711ea4b7453400
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-31 21:52:23 -0700

    first commit

--- lib/view/screen/profile/profile_edit.dart
+++ lib/view/screen/profile/profile_edit.dart
@@ -5,6 +5,10 @@ class ProfileEdit extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile Edit"),
+      ),
+    );
   }
 }
