233e12cbaf1e469a1f678d113d0b6db3e98f8143
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-31 22:08:32 -0700

    first commit

--- lib/controller/ui_controller/profile/profile.dart
+++ lib/controller/ui_controller/profile/profile.dart
@@ -1,15 +1,14 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
-  final TextEditingController fastnameController =TextEditingController();
-  final TextEditingController lastController =TextEditingController();
-  final TextEditingController phoneController =TextEditingController();
-  final TextEditingController emailController =TextEditingController();
+  final TextEditingController fastnameController = TextEditingController();
+  final TextEditingController lastController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
   var fastname = ''.obs;
   var lastname = ''.obs;
+   var phone = ''.obs;
 
   void updateName(String newName) {
     fastname.value = newName;
@@ -17,5 +16,9 @@ class ProfileController extends GetxController {
 
   void updatelastName(String newPhone) {
     lastname.value = newPhone;
+  }
+
+  void updatePhone(String newPhone) {
+    phone.value = newPhone;
   }
 }
