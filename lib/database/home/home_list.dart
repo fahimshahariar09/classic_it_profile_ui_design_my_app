2fe8fd15ba0f9bbe25aad8a43b4790f21451aaeb
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-02 19:28:41 -0700

    first commit

--- lib/database/home/home_list.dart
+++ lib/database/home/home_list.dart
@@ -1,10 +1,10 @@
 class HomeList {
   static Map<String, dynamic> homelist = {
     "home": [
-      {"image": "asset/food/fish.png", "name": "Fish Food", "price": "150",},
-      {"image": "asset/food/fish.png", "name": "cat", "price": "150", "id": ""},
-      {"image": "asset/food/fish.png", "name": "dog", "price": "100", "id": ""},
-      {"image": "asset/food/fish.png", "name": "cow", "price": "110", "id": ""},
+      {"image": "asset/food/fish.png", "name": "Fish Food", "price": "150","id": "1"},
+      {"image": "asset/food/fish.png", "name": "cat", "price": "150", "id": "2"},
+      {"image": "asset/food/fish.png", "name": "dog", "price": "100", "id": "3"},
+      {"image": "asset/food/fish.png", "name": "cow", "price": "110", "id": "4"},
     ]
   };
 }
