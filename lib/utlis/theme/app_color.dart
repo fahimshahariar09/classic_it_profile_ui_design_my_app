de4faeffc6f423f4ab3240fbe93ba7c174501322
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-04 22:25:15 -0800

    first commit

---
+++ lib/utlis/theme/app_color.dart
@@ -0,0 +1,9 @@
+
+import 'dart:ui';
+
+class AppColors{
+  //light Theme
+  static Color bgLightColors = Color(0xffFFFFFF);
+  static Color bg1LightColors = Color(0xffFF6321);
+  static Color bg2LightColors = Color(0xffFF8450);
+}
