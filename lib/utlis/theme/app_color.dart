67089dc824475e0691bd332e8bb01d15a6a9ed4a
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-04 22:30:26 -0800

    first commit

--- lib/utlis/theme/app_color.dart
+++ lib/utlis/theme/app_color.dart
@@ -3,7 +3,18 @@ import 'dart:ui';
 
 class AppColors{
   //light Theme
-  static Color bgLightColors = Color(0xffFFFFFF);
-  static Color bg1LightColors = Color(0xffFF6321);
-  static Color bg2LightColors = Color(0xffFF8450);
+  static const Color bgLightColors =  Color(0xffFFFFFF);
+  static const Color primaryLightColor =  Color(0xffFFFFFF);
+  static const Color cardLightColor =  Color(0xffFEFEFE);
+  static const Color cardGreyColor =  Color(0x7effffff);
+  static const Color appbarLightColor =  Color(0xff015BC1);
+  static const Color textLightColor =  Color(0xff1A2E35);
+  static const Color bg1LightColors =  Color(0xffFF6321);
+  static const Color bg2LightColors =  Color(0xffFF8450);
+  
+  //dark Theme
+
+static const Color cardDarkColor = Color(0xff2c394d);
+static const Color textDarkColor = Color(0xffffffff);
+
 }
