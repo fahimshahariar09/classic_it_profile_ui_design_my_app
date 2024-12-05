772f95cc0709180fdc23f372b482343f1f41cb69
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-04 22:37:09 -0800

    first commit

--- lib/utlis/setting/easy_loading.dart
+++ lib/utlis/setting/easy_loading.dart
@@ -1,4 +1,20 @@
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:myapp/utlis/theme/app_color.dart';
+
 class EasyLoadingSettings{
 
+  easyloadingSetting(){
+    EasyLoading.instance
+        ..displayDuration = Duration(milliseconds: 500)
+        ..loadingStyle = EasyLoadingStyle.custom
+        ..backgroundColor =AppColors.bg1LightColors
+        ..indicatorColor = AppColors.bgLightColors
+        ..maskColor =AppColors.bg1LightColors
+        ..textColor =AppColors.bgLightColors
+        ..contentPadding =EdgeInsets.symmetric(horizontal: 50,vertical: 20)
+        ..indicatorType=EasyLoadingIndicatorType.fadingGrid
+        ..userInteractions =
+  }
 }
