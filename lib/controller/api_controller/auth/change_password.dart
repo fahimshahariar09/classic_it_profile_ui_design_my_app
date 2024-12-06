fa407f157a9a4f264ac14e0f537dbb7f429e292f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-05 21:20:36 -0800

    first commit

--- lib/controller/api_controller/auth/change_password.dart
+++ lib/controller/api_controller/auth/change_password.dart
@@ -4,7 +4,11 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class ChangePasswordService {
   static Future<bool> changepasswordService() async {
-    try {} catch (e) {
+    try {
+      await Future.delayed(const Duration(seconds: 2));
+      await EasyLoading.showSuccess("changed");
+      return true;
+    } catch (e) {
       log("error $e");
     }
     await EasyLoading.showError("something went wrong...!!");
