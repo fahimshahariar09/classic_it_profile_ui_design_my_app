c8c7031d54dfc4606c1ad87797c235c7208abc27
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-05 21:25:44 -0800

    first commit

--- lib/controller/api_controller/auth/log_out.dart
+++ lib/controller/api_controller/auth/log_out.dart
@@ -1,5 +1,15 @@
-class LogOutService{
-  static Future logoutService()async{
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
+class LogOutService {
+  static Future<bool> logoutService() async {
+    try {
+      await EasyLoading.showSuccess("Log Out Successful");
+      return true;
+    } catch (e) {
+      debugPrint("error $e");
+    }
+    await EasyLoading.showError("something went wrong...!");
+    return false;
   }
 }
