a1ab1766da425f106132c2111459b11e11c77f2e
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-05 21:37:36 -0800

    first commit

--- lib/controller/api_controller/auth/profile_update.dart
+++ lib/controller/api_controller/auth/profile_update.dart
@@ -1,3 +1,23 @@
-class ProfileUpdateService{
+import 'package:flutter/cupertino.dart';
 
+class ProfileUpdateService {
+  static Future<bool> profileupdateService() async {
+    try {
+      await Future.delayed(Duration(seconds: 2));
+
+      var data ={
+        "id":4,
+        "name":"fahim",
+        "username":"null",
+        "email":"fahim@gmail",
+        "phone":01547952526,
+        "address":"address",
+        "phone":"",
+      };
+
+    } catch (e) {
+      debugPrint("error $e");
+    }
+    return false;
+  }
 }
