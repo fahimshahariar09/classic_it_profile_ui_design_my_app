b5c52a95d3f7c02242270af1e982a0a4ac994f48
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-06 22:08:35 -0800

    first commit

---
+++ lib/view/common_widget/image_view_widget.dart
@@ -0,0 +1,8 @@
+class ImageViewWidget extends StatelessWidget {
+  const ImageViewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
