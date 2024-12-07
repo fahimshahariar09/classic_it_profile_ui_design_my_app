4683090b39acb9d77f9d6f7f724e8cc61f61145d
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-06 22:27:27 -0800

    first commit

---
+++ lib/view/screen/profile/widget/log_out.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/view/common_widget/custom_text.dart';
+
+class LogOut extends StatelessWidget {
+  const LogOut({super.key, required this.onTap});
+
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: const Card(
+        elevation: 4,
+        surfaceTintColor: Colors.transparent,
+        shape: OutlineInputBorder(borderSide: BorderSide.none),
+        child: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10,vertical: 15),
+              child: CustomTextWidget(text: "log out"),
+            ),
+            Icon(Icons.logout,color: Colors.black54,)
+          ],
+        ),
+      ),
+    );
+  }
+}
