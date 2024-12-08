01c0c7d99db7b4ee0ecaa46b3d370da5849a90d3
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-07 22:08:56 -0800

    first commit

--- lib/controller/ui_controller/profile/profile.dart
+++ lib/controller/ui_controller/profile/profile.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfileController extends GetxController {
+  final formKey = GlobalKey<FormState>();
   TextEditingController nameController = TextEditingController();
   TextEditingController mailController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
@@ -9,4 +13,16 @@ class ProfileController extends GetxController {
   TextEditingController oldPasswordController = TextEditingController();
   TextEditingController newPasswordController = TextEditingController();
   TextEditingController confPasswordController = TextEditingController();
+  RxInt selectedTab = 0.obs;
+  RxInt selectedOrderTab = 1.obs;
+  RxBool isLoading = false.obs;
+  RxMap<dynamic, dynamic> userInfo = {}.obs;
+  ImageSource imageSource = ImageSource.camera;
+  late Rx<File?> profileIMG = Rx<File?>(null);
+  final picker = ImagePicker();
+  @override
+  void onInit() {
+
+    super.onInit();
+  }
 }
