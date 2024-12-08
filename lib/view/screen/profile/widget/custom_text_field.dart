c93cdfc55c4fb0f66d37a106dbbdd4810d181cd5
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-07 21:48:50 -0800

    first commit

--- lib/view/screen/profile/widget/custom_text_field.dart
+++ lib/view/screen/profile/widget/custom_text_field.dart
@@ -3,10 +3,14 @@ import 'package:myapp/utlis/theme/app_color.dart';
 
 class CustomTextField extends StatelessWidget {
   const CustomTextField(
-      {super.key, required this.controller, this.textInputType});
+      {super.key,
+      required this.controller,
+      this.textInputType,
+      required this.labelText});
 
   final TextEditingController controller;
   final TextInputType? textInputType;
+  final String labelText;
 
   @override
   Widget build(BuildContext context) {
@@ -16,17 +20,24 @@ class CustomTextField extends StatelessWidget {
         controller: controller,
         keyboardType: textInputType ?? TextInputType.text,
         decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(horizontal: 10),
-          enabledBorder: OutlineInputBorder(
+          contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+          labelText: labelText,
+          enabledBorder: const OutlineInputBorder(
               borderSide: BorderSide(color: AppColors.cardDarkColor)),
-          border: OutlineInputBorder(
+          border: const OutlineInputBorder(
               borderSide: BorderSide(color: AppColors.cardDarkColor)),
-          errorBorder: OutlineInputBorder(
+          errorBorder: const OutlineInputBorder(
               borderSide: BorderSide(color: AppColors.cardDarkColor)),
-          errorStyle: TextStyle(fontSize: 12),
-          focusedBorder: OutlineInputBorder(
+          errorStyle: const TextStyle(fontSize: 12),
+          focusedBorder: const OutlineInputBorder(
               borderSide: BorderSide(color: AppColors.bg1LightColor)),
         ),
+        validator: (value) {
+          if (value.toString() == "") {
+            return "Field can't be empty";
+          }
+          return null;
+        },
       ),
     );
   }
