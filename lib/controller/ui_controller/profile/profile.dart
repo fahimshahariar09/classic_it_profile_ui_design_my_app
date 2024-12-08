440b5e62b14ec90317616f403fc43533d96e8839
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-07 21:55:25 -0800

    first commit

--- lib/controller/ui_controller/profile/profile.dart
+++ lib/controller/ui_controller/profile/profile.dart
@@ -2,28 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
-  final TextEditingController fastnameController = TextEditingController();
-  final TextEditingController lastController = TextEditingController();
-  final TextEditingController phoneController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
-  var fastname = ''.obs;
-  var lastname = ''.obs;
-  var phone = ''.obs;
-  var email = ''.obs;
-
-  void updateName(String newName) {
-    fastname.value = newName;
-  }
-
-  void updatelastName(String newPhone) {
-    lastname.value = newPhone;
-  }
-
-  void updatePhone(String newPhone) {
-    phone.value = newPhone;
-  }
-
-  void updateEmail(String newPhone) {
-    email.value = newPhone;
-  }
+  TextEditingController nameController = TextEditingController();
+  TextEditingController mailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+  TextEditingController oldPasswordController = TextEditingController();
+  TextEditingController newPasswordController = TextEditingController();
+  TextEditingController confPasswordController = TextEditingController();
 }
