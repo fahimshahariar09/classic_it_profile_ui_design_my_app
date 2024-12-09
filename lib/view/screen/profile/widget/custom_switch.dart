f627338de8d521f451a7c98ef65855f0bbee34a2
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-08 23:36:49 -0800

    first commit

--- lib/view/screen/profile/widget/custom_switch.dart
+++ lib/view/screen/profile/widget/custom_switch.dart
@@ -4,10 +4,15 @@ import 'package:myapp/view/common_widget/custom_text.dart';
 
 class CustomSwitch extends StatelessWidget {
   const CustomSwitch(
-      {super.key, required this.onTap, this.value, required this.onText});
+      {super.key,
+      required this.onTap,
+      this.value,
+      required this.onText,
+      required this.offText});
 
   final VoidCallback onTap;
   final String onText;
+  final String offText;
   final dynamic value;
 
   @override
@@ -16,22 +21,53 @@ class CustomSwitch extends StatelessWidget {
       onTap: onTap,
       child: Container(
         height: 25,
-        width: 30,
-        alignment: Alignment.center,
         decoration: BoxDecoration(
-          color: value.value == false
-              ? AppColors.bg1LightColor
-              : Theme.of(context).cardColor,
           borderRadius: BorderRadius.circular(5),
-          border: Border.all(color: Colors.transparent),
+          border: Border.all(color: Colors.grey),
         ),
-        child: CustomTextWidget(
-          text: onText,
-          fontColor: value.value == false
-              ? AppColors.bg1LightColor
-              : AppColors.bgLightColor,
-          fontWeight: FontWeight.w600,
-          fontSize: 14,
+        child: Row(
+          children: [
+            Container(
+              height: 25,
+              width: 30,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: value.value == false
+                    ? AppColors.bg1LightColor
+                    : Theme.of(context).cardColor,
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: Colors.transparent),
+              ),
+              child: CustomTextWidget(
+                text: onText,
+                fontColor: value.value == false
+                    ? AppColors.bg1LightColor
+                    : AppColors.bgLightColor,
+                fontWeight: FontWeight.w600,
+                fontSize: 14,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 25,
+              width: 30,
+              decoration: BoxDecoration(
+                color: value.value == false
+                    ? AppColors.bg1LightColor
+                    : AppColors.bgLightColor,
+                border: Border.all(color: Colors.transparent),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: CustomTextWidget(
+                text: offText,
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+                fontColor: value.value == false
+                    ? AppColors.bg1LightColor
+                    : Theme.of(context).cardColor,
+              ),
+            )
+          ],
         ),
       ),
     );
