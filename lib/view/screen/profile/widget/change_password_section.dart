b5ba7b28e572af2b5e59ca3836bee84afb4c2cd9
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-08 22:58:06 -0800

    first commit

--- lib/view/screen/profile/widget/change_password_section.dart
+++ lib/view/screen/profile/widget/change_password_section.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:myapp/controller/ui_controller/profile/profile.dart';
 
 class ChangePasswordSection extends StatelessWidget {
   const ChangePasswordSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    ProfileController profileController = Get.put(ProfileController());
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10, right: 10, left: 10, top: 10),
+      child: SingleChildScrollView(
+        child: Form(
+          key: profileController.formKey,
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
