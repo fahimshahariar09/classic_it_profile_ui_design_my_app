5ea751683dfdd5a26f8206d42571328d7e72a001
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-09 22:56:11 -0800

    first commit

---
+++ lib/view/screen/profile/widget/deleted_account_section.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class DeletedAccountSection extends StatelessWidget {
+  const DeletedAccountSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
