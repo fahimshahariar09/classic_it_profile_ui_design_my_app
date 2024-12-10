dcac2f0f5f97292e8df7a00c51d021cc25da7a97
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-09 23:10:01 -0800

    first commit

--- lib/view/screen/profile/widget/deleted_account_section.dart
+++ lib/view/screen/profile/widget/deleted_account_section.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapp/controller/ui_controller/profile/profile.dart';
+import 'package:myapp/utlis/theme/app_color.dart';
+import 'package:myapp/view/common_widget/custom_button.dart';
+import 'package:myapp/view/screen/profile/widget/custom_text_field.dart';
 
 class DeletedAccountSection extends StatelessWidget {
   const DeletedAccountSection({super.key});
@@ -17,7 +20,22 @@ class DeletedAccountSection extends StatelessWidget {
             children: [
               Row(
                 children: [
-
+                  const Icon(
+                    Icons.account_circle,
+                    color: AppColors.bg1LightColor,
+                  ),
+                  const SizedBox(height: 10),
+                  CustomTextField(
+                    controller: profileController.oldPasswordController,
+                    labelText: "current_password",
+                  ),
+                  SizedBox(height: 10),
+                  CustomButton(
+                      text: "deleted",
+                      buttonWidth: 150,
+                      onTap: () async {
+                        FocusScope.of(context).unfocus();
+                      })
                 ],
               )
             ],
