b06321ba69b1a57c807d907668f15b064cb9f0da
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-09 22:53:39 -0800

    first commit

--- lib/view/screen/profile/widget/setting_list_tile.dart
+++ lib/view/screen/profile/widget/setting_list_tile.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/view/common_widget/custom_text.dart';
 
 class SettingListTile extends StatelessWidget {
-  const SettingListTile({super.key, required this.onTap});
+  const SettingListTile({super.key, required this.onTap, required this.titel});
 
   final VoidCallback onTap;
+  final String titel;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
+      child: Card(
+        surfaceTintColor: Colors.transparent,
+        shape: const OutlineInputBorder(borderSide: BorderSide.none),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CustomTextWidget(text: titel),
+            const Icon(
+              Icons.arrow_forward_ios_sharp,
+              size: 17,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
