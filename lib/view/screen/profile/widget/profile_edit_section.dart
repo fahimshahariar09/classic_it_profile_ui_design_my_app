ca54bb4e229846e2d696683ba68b5ae507c0357d
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-09 23:43:39 -0800

    first commit

--- lib/view/screen/profile/widget/profile_edit_section.dart
+++ lib/view/screen/profile/widget/profile_edit_section.dart
@@ -4,7 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:myapp/controller/ui_controller/profile/profile.dart';
+import 'package:myapp/utlis/common_funcation/common_snackbar_message.dart';
+import 'package:myapp/utlis/common_funcation/internet_connection%20_check.dart';
+import 'package:myapp/view/common_widget/custom_button.dart';
 import 'package:myapp/view/common_widget/custom_text.dart';
+import 'package:myapp/view/screen/profile/widget/custom_text_field.dart';
 
 class ProfileEditSection extends StatelessWidget {
   const ProfileEditSection({super.key});
@@ -50,7 +54,29 @@ class ProfileEditSection extends StatelessWidget {
                       )
                     ],
                   ),
-                )
+                ),
+                const SizedBox(height: 10),
+                CustomTextField(
+                  controller: profileController.nameController,
+                  labelText: "name",
+                ),
+                CustomTextField(
+                  controller: profileController.addressController,
+                  labelText: "address",
+                ),
+                const SizedBox(height: 10),
+                CustomButton(text: "edit", onTap: ()async{
+                  FocusScope.of(context).unfocus();
+                  if(!await ConnectionChecker.connectionChecker()){
+                    CommonSnackbarMessage.noInternetConnection();
+                    return;
+                  }
+                  bool status = await profileController.profileUpdateFuncation();
+                  if(status){
+                    await profileController.getProfileInfo();
+                    Get.back();
+                  }
+                })
               ],
             )
           ],
