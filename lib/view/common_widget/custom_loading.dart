3d32264eac1fe76fc5c92191b853f9506be49ab5
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-09 23:14:36 -0800

    first commit

--- lib/view/common_widget/custom_loading.dart
+++ lib/view/common_widget/custom_loading.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/utlis/theme/app_color.dart';
 
 class CustomLoading extends StatelessWidget {
   const CustomLoading({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card();
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 15),
+      color: AppColors.bg1LightColor,
+      shape: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+        borderSide: BorderSide.none,
+      ),
+      child: SizedBox(
+        height: 35,
+        width: MediaQuery.sizeOf(context).width,
+        child: const CircularProgressIndicator(
+          backgroundColor: Colors.white,
+          color: AppColors.bg1LightColor,
+        ),
+      ),
+    );
   }
 }
