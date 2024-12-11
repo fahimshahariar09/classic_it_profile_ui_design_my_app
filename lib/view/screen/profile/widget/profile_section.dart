9ff8322819962d21135cf7c7348fb3c2b4fe3a49
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-10 23:25:44 -0800

    first commit

--- lib/view/screen/profile/widget/profile_section.dart
+++ lib/view/screen/profile/widget/profile_section.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/view/screen/profile/widget/setting_list_tile.dart';
 
 class ProfileSection extends StatelessWidget {
   const ProfileSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SizedBox(
+      child: SingleChildScrollView(
+        child: Padding(padding: EdgeInsets.all(5),
+        child: Column(
+          children: [
+            SettingListTile(onTap: (){}, titel: "change_password")
+          ],
+        ),),
+      ),
+    );
   }
 }
