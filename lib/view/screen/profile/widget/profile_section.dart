e04bca0f56aab4cea1b901f1e0536d954ed802b8
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-10 23:30:30 -0800

    first commit

--- lib/view/screen/profile/widget/profile_section.dart
+++ lib/view/screen/profile/widget/profile_section.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/view/common_widget/custom_text.dart';
+import 'package:myapp/view/screen/profile/widget/change_password_section.dart';
 import 'package:myapp/view/screen/profile/widget/setting_list_tile.dart';
 
 class ProfileSection extends StatelessWidget {
@@ -8,12 +10,39 @@ class ProfileSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       child: SingleChildScrollView(
-        child: Padding(padding: EdgeInsets.all(5),
-        child: Column(
-          children: [
-            SettingListTile(onTap: (){}, titel: "change_password")
-          ],
-        ),),
+        child: Padding(
+          padding: const EdgeInsets.all(5),
+          child: Column(
+            children: [
+              //change password
+              SettingListTile(
+                  onTap: () {
+                    showModalBottomSheet(
+                        context: context,
+                        builder: (builder) {
+                          return const ChangePasswordSection();
+                        });
+                  },
+                  titel: "change_password"),
+              //change password
+               Card(
+                elevation: 4,
+                surfaceTintColor: Colors.transparent,
+                shape: OutlineInputBorder(borderSide: BorderSide.none),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 15),
+                      child: CustomTextWidget(text: "language change"),
+                    ),
+
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
