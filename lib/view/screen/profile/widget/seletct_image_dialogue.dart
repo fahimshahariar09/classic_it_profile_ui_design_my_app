30edd29a325205554fe17e8bb0d5ab96a58fce6c
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-11 21:59:41 -0800

    first commit

--- lib/view/screen/profile/widget/seletct_image_dialogue.dart
+++ lib/view/screen/profile/widget/seletct_image_dialogue.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/utlis/theme/app_color.dart';
 import 'package:myapp/view/common_widget/custom_text.dart';
 
 class SeletctImageDialogue extends StatelessWidget {
-  const SeletctImageDialogue({super.key});
+  const SeletctImageDialogue(
+      {super.key, required this.cameraButton, required this.galleryButton});
+
+  final VoidCallback cameraButton;
+  final VoidCallback galleryButton;
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +15,32 @@ class SeletctImageDialogue extends StatelessWidget {
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          CustomTextWidget(
+          const CustomTextWidget(
             text: "Select Image from",
             fontSize: 20,
           ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              InkWell(
+                onTap: cameraButton,
+                child: const Icon(
+                  Icons.camera_alt,
+                  size: 30,
+                  color: AppColors.bg1LightColor,
+                ),
+              ),
+              InkWell(
+                onTap: galleryButton,
+                child: const Icon(
+                  Icons.photo,
+                  size: 30,
+                  color: AppColors.bg1LightColor,
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
