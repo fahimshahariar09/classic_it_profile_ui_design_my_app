17c7a855c20da9b00ab06c859a2bbcb5ae359d41
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-11 22:20:19 -0800

    first commit

--- lib/view/screen/profile/profile.dart
+++ lib/view/screen/profile/profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapp/controller/ui_controller/profile/profile.dart';
 import 'package:myapp/utlis/theme/app_color.dart';
+import 'package:myapp/view/common_widget/image_view_widget.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -22,12 +23,42 @@ class ProfilePage extends StatelessWidget {
                   width: size.width,
                   padding: const EdgeInsets.only(top: 70, bottom: 20),
                   decoration: const BoxDecoration(
-                      color: AppColors.bg1LightColor,
-                      borderRadius: BorderRadius.only(
-                        bottomRight: Radius.circular(20),
-                        bottomLeft: Radius.circular(20),
-                      )),
-                )
+                    color: AppColors.bg1LightColor,
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Get.to(
+                            ImageViewWidget(
+                                imageurl:
+                                    "${profileController.userInfo['profile_image'] ?? ''}"),
+                          );
+                        },
+                        child: SizedBox(
+                          height: 100,
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: ClipOval(
+                              child: Obx(
+                                () => FadeInImage.assetNetwork(
+                                    height: 100,
+                                    width: 100,
+                                    placeholder: "asset/food/fish.png",
+                                    image:
+                                        "${profileController.userInfo['profile_image'] ?? ''}"),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             )
           ],
