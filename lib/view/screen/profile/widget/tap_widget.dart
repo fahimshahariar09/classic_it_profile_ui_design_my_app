f52fde9d4e5106bda774c1cb47985d4e9c8e946c
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-12 23:30:55 -0800

    first commit

--- lib/view/screen/profile/widget/tap_widget.dart
+++ lib/view/screen/profile/widget/tap_widget.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/utlis/theme/app_color.dart';
 
 class TapWidget extends StatelessWidget {
-  const TapWidget({super.key, required this.onTap, this.value});
+  const TapWidget(
+      {super.key,
+      required this.onTap,
+      this.value,
+      required this.selectedIndex,
+      required this.titel});
 
   final VoidCallback onTap;
   final dynamic value;
+  final int selectedIndex;
+  final String titel;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Card(),
+      child: Card(
+        color: value.value == selectedIndex
+            ? AppColors.bgLightColor
+            : AppColors.bg1LightColor,
+      ),
     );
   }
 }
